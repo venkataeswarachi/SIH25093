afe2b0b448af09aa33d28fa380e17f26e11e288f
Bharath Gadde <bharathbannu999@gmail.com>
2026-03-01 15:50:20 +0530

    Fixed bugs

--- University/src/main/java/com/vvit/University/UniversityApplication.java
+++ University/src/main/java/com/vvit/University/UniversityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniversityApplication {
-
+	//Main execution starts here
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityApplication.class, args);
 	}
