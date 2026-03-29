758cd9a8ea2f145e3f3e3162a41a6b56f7cd249a
Venkata Eswar <Venkataeswar2004@gmail.com>
2026-03-29 09:54:14 +0530

    Added .env in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
-
+import dotenv from 'dotenv';
 const api = axios.create({
     // Using 127.0.0.1 instead of localhost avoids IPv6 (::1) resolution issues
-    baseURL: 'http://127.0.0.1:2008', 
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:2008',
     headers: {
         'Content-Type': 'application/json',
     },
